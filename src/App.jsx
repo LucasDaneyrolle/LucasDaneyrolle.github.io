d8051ac9dd4bb705677096e4f8c5da07f3f3fcce
LucasDaneyrolle <lucas.ddu69@gmail.com>
2024-02-08 17:03:34 +0100

    setup env

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="body-content">
+      <div className="body-content__title">Portfolio </div>
+      <div className="body-content__main">Actuellement en travaux...</div>
+    </div>
+  )
+}
+
+export default App
